53a9bee249065c9f50d70426cb5ea16043f31135
sumit kumar <sumit1523@gmail.com>
2024-08-13 17:10:00 +0530

    volvoslider initial code from official sample

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@volvo-cars/css/font-face.css";
+import "@volvo-cars/css/tokens.css";
+import "@volvo-cars/css/styles_all-media.css";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import VolvoSlider from "./VolvoSlider";
+
 export default function Home() {
-  return <div>VOLVO CARS</div>;
+  return (
+    <div className="container-max">
+      <h1 className="heading-1">Volvo Car For a better future</h1>
+      <VolvoSlider />
+    </div>
+  );
 }
